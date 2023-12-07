bc2ef77aa394c021ef37760cd5a8b62f123620a6
ankitadulkar <ankit.adulkar1@justdial.com>
2023-12-07 22:59:39 +0530

    added link in a tag

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -9,52 +9,52 @@ export default function Footer() {
                 <div className={`container mx-auto flex flex-col justify-between`}>
                     <div className={`flex justify-between ${styles.footer__top}`}>
                         <div className={`flex-1 w-25 mr-20 ${styles.footer__left}`}>
-                            <a href='www.google.com' className={`bg-no-repeat bg-center bg-cover inline-block ${styles.footer__logo}`} />
+                            <a href='#' className={`bg-no-repeat bg-center bg-cover inline-block ${styles.footer__logo}`}></a>
                             <p className={`text-white font-medium mt-5 mb-5`}>EnthuZiastic is a vibrant and dynamic community dedicated to fostering a passion for learning and personal growth. </p>
                             <ul className={`flex items-center gap-5`}>
                                 <li>
-                                    <a href='www.google.com' className={`bg-no-repeat bg-center bg-cover inline-block ${styles.linkedin}`} />
+                                    <a href='#' className={`bg-no-repeat bg-center bg-cover inline-block ${styles.linkedin}`} ></a>
                                 </li>
                                 <li>
-                                    <a href='www.google.com' className={`bg-no-repeat bg-center bg-cover inline-block ${styles.twitter}`} />
+                                    <a href='#' className={`bg-no-repeat bg-center bg-cover inline-block ${styles.twitter}`} ></a>
                                 </li>
                                 <li>
-                                    <a href='www.google.com' className={`bg-no-repeat bg-center bg-cover inline-block ${styles.facebook}`} />
+                                    <a href='#' className={`bg-no-repeat bg-center bg-cover inline-block ${styles.facebook}`} ></a>
                                 </li>
                                 <li>
-                                    <a href='www.google.com' className={`bg-no-repeat bg-center bg-cover inline-block ${styles.youtube}`} />
+                                    <a href='#' className={`bg-no-repeat bg-center bg-cover inline-block ${styles.youtube}`} ></a>
                                 </li>
                             </ul>
                         </div>
                         <div className={`flex-auto w-70 flex ${styles.footer__right}`}>
                             <ul className={`flex-auto w-25`}>
                                 <li><b className={`text-white font-extrabold text-2xl`}>COMPANY</b></li>
-                                <li><a className={`text-white`} href='www.google.com'>About</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>Premium</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>Blog</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>Affiliate Program</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>Get Coupon</a></li>
+                                <li><a className={`text-white`} href="#">About</a></li>
+                                <li><a className={`text-white`} href="#">Premium</a></li>
+                                <li><a className={`text-white`} href="#">Blog</a></li>
+                                <li><a className={`text-white`} href="#">Affiliate Program</a></li>
+                                <li><a className={`text-white`} href="#">Get Coupon</a></li>
                             </ul>
                             <ul className={`flex-auto w-25`}>
                                 <li><b className={`text-white font-extrabold text-2xl`}>HELP AND SUPPORT</b></li>
-                                <li><a className={`text-white`} href='www.google.com'>Contact Us</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>Knowledge Center</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>Premium Support</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>Sponsorships</a></li>
+                                <li><a className={`text-white`} href="#">Contact Us</a></li>
+                                <li><a className={`text-white`} href="#">Knowledge Center</a></li>
+                                <li><a className={`text-white`} href="#">Premium Support</a></li>
+                                <li><a className={`text-white`} href="#">Sponsorships</a></li>
                             </ul>
                             <ul className={`flex-auto w-25`}>
                                 <li><b className={`text-white font-extrabold text-2xl`}>RESOURCES</b></li>
-                                <li><a className={`text-white`} href='www.google.com'>Third-Party Tools</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>Illustrations</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>Themesberg</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>Bluehost</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>Stock Photos</a></li>
+                                <li><a className={`text-white`} href="#">Third-Party Tools</a></li>
+                                <li><a className={`text-white`} href="#">Illustrations</a></li>
+                                <li><a className={`text-white`} href="#">Themesberg</a></li>
+                                <li><a className={`text-white`} href="#">Bluehost</a></li>
+                                <li><a className={`text-white`} href="#">Stock Photos</a></li>
                             </ul>
                             <ul className={`flex-auto w-25`}>
                                 <li><b className={`text-white font-extrabold text-2xl`}>LEGAL</b></li>
-                                <li><a className={`text-white`} href='www.google.com'>Privacy Policy</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>Terms &amp; Conditions</a></li>
-                                <li><a className={`text-white`} href='www.google.com'>EULA</a></li>
+                                <li><a className={`text-white`} href="#">Privacy Policy</a></li>
+                                <li><a className={`text-white`} href="#">Terms &amp; Conditions</a></li>
+                                <li><a className={`text-white`} href="#">EULA</a></li>
                             </ul>
                         </div>
                     </div>
